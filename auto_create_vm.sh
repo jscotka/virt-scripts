ec8838f306953bd18f03dd1463dd5350b1a104b5
Jan Scotka <jscotka@redhat.com>
2015-03-19 10:01:16 +0100

    added timeout and dir option optional

--- auto_create_vm.sh
+++ auto_create_vm.sh
@@ -4,19 +4,19 @@ prog=`basename $0`
 
 function printh() {
     echo -e "Usage: $prog "
-    echo " <postfix> <dir> [customization_script]"
+    echo " <postfix> [dir] [customization_script]"
     echo "    postfix:                it create output name in format like fedora21-suffix"
-    echo "    dir:                  where to create/store images"
+    echo "    dir:                  where to create/store images [optional]"
     echo "    customization_script: do customization script on guests [optional]"
 
 }
-if [ "$#" -lt 2 ]; then
+if [ "$#" -lt 1 ]; then
     printh;
     exit 255
 fi
 
 POSTFIX=$1
-DIR=$2
+test "$2" && DIR=$2 || DIR=/var/lib/libvirt/images
 test "$3" && CUSTOMIZATION="--script '$3'"
 ARCH=x86_64
 TYPE=qcow2
@@ -24,12 +24,14 @@ mkdir -p $DIR
 
 
 function create_parametrized(){
+set -x
     DIST=$1
     REALDIST=$2
     REVISION=$3
     test "$4" && DISTOVERWRITE=$4 || DISTOVERWRITE=$DIST
     DOMAIN=$DIST-$POSTFIX
     ./create-guest-qcow2.bash $DOMAIN $REALDIST $ARCH $DIR
+    sleep 5
     sudo virt-sysprep --connect qemu:///system -d $DOMAIN $CUSTOMIZATION
     DISCNAME="$DOMAIN.$TYPE"
     CDISCNAME="$DISCNAME.xz"
@@ -37,7 +39,6 @@ function create_parametrized(){
     xz --best --block-size=16777216 $DIR/$DISCNAME
     COMSIZE=`stat $DIR/$CDISCNAME -c "%s"`
     SHASUM=`sha512sum $DIR/$CDISCNAME | cut -d ' ' -f 1`
-
     cat <<EOF >> $DIR/index
 [$DOMAIN]
 name=$DOMAIN
@@ -50,6 +51,7 @@ format=$TYPE
 size=$UNSIZE
 compressed_size=$COMSIZE
 EOF
+set +x
 }
 
 create_parametrized fedora21 f21 1 fedora21
